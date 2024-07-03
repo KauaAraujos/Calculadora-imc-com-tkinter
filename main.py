bd1d92a8a8b61adffa901d7cbe67f49f593ee3f5
Kauã Araujo <kauavictordearaujos@gmail.com>
2024-07-03 11:55:53 -0300

    função de calcular feita

--- main.py
+++ main.py
@@ -8,16 +8,11 @@ class Janela:
         self.janela.resizable(False,False)
         self.home()
         
-    def home(self):
-        texto_altura = 'Sua altura em cm*'
-        texto_peso = 'Seu peso*'
         
-        def limpar_texto():
-            self.pegar_peso = CTkEntry(self.frame, width=120,border_color='#d0e0eb', placeholder_text=texto_peso)
-            self.pegar_peso.place(x=50, y=140)
-            
-            self.pegar_altura = CTkEntry(self.frame, width=120, placeholder_text=texto_altura,border_color='#d0e0eb')
-            self.pegar_altura.place(x=50, y=70)
+    def home(self):
+        self.texto_altura = 'Sua altura em cm*'
+        self.texto_peso = 'Seu peso*'
+        
         
         self.frame = CTkFrame(self.janela, fg_color='#070743',width=220, height=220)
         self.frame.grid(row=0, column=0)
@@ -31,20 +26,39 @@ class Janela:
         self.nome_altura = CTkLabel(self.frame, text='Altura', font=('Arial',13))
         self.nome_altura.place(x=54, y=47)
 
-        self.pegar_altura = CTkEntry(self.frame, width=120, placeholder_text=texto_altura,border_color='#d0e0eb')
+        self.pegar_altura = CTkEntry(self.frame, width=120, placeholder_text=self.texto_altura,border_color='#d0e0eb')
         self.pegar_altura.place(x=50, y=70)
         
         self.nome_peso = CTkLabel(self.frame, text='Peso', font=('Arial',13))
         self.nome_peso.place(x=54, y=117)
         
-        self.pegar_peso = CTkEntry(self.frame, width=120,border_color='#d0e0eb', placeholder_text=texto_peso)
+        self.pegar_peso = CTkEntry(self.frame, width=120,border_color='#d0e0eb', placeholder_text=self.texto_peso)
         self.pegar_peso.place(x=50, y=140)
         
-        self.botao_calcular = CTkButton(self.frame, text_color='#12122b',fg_color='#88abc2', text='Calcular', width=30, height=25)
+        self.botao_calcular = CTkButton(self.frame, text_color='#12122b',fg_color='#88abc2', text='Calcular', width=30, height=25, command=self.calcularImc)
         self.botao_calcular.place(x=50, y=190)
         
-        self.botao_limpar = CTkButton(self.frame,text_color='#12122b',fg_color='#88abc2',text='Limpar', width=30, height=25, command=limpar_texto)
+        self.botao_limpar = CTkButton(self.frame,text_color='#12122b',fg_color='#88abc2',text='Limpar', width=30, height=25, command=self.limpar_texto)
         self.botao_limpar.place(x=120, y=190)
+        
+    def calcularImc(self):
+        try:
+            self.alturab = self.pegar_altura.get()
+            self.pesob = self.pegar_peso.get()
+            self.peso = float(self.pesob)
+            self.altura = float(self.alturab)
+            self.imc = self.peso / (self.altura ** 2)
+            print(self.imc)
+        except:
+            pass
+        
+       
+    def limpar_texto(self):
+        self.pegar_peso = CTkEntry(self.frame, width=120,border_color='#d0e0eb', placeholder_text=self.texto_peso)
+        self.pegar_peso.place(x=50, y=140)
+        
+        self.pegar_altura = CTkEntry(self.frame, width=120, placeholder_text=self.texto_altura,border_color='#d0e0eb')
+        self.pegar_altura.place(x=50, y=70)
         
 if __name__ == '__main__':
     app = CTk()
