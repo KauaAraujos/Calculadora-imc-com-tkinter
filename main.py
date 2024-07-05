4a850042d298307fdc2508d2a1456658217cce3f
Kauã Araujo <kauavictordearaujos@gmail.com>
2024-07-05 12:29:59 -0300

    Finalizado

--- main.py
+++ main.py
@@ -36,15 +36,20 @@ class Janela:
         self.pegar_peso = CTkEntry(self.frame, width=120,border_color='#d0e0eb', placeholder_text=self.texto_peso)
         self.pegar_peso.place(x=50, y=140)
         
-        self.botao_calcular = CTkButton(self.frame, text_color='#12122b',fg_color='#88abc2', text='Calcular', width=30, height=25, command=self.classificarImc)
+        self.botao_calcular = CTkButton(self.frame, text_color='#12122b',fg_color='#88abc2', text='Calcular', width=30, height=25, command=self.mostrarEstadoImc)
         self.botao_calcular.place(x=50, y=190)
         
         self.botao_limpar = CTkButton(self.frame,text_color='#12122b',fg_color='#88abc2',text='Limpar', width=30, height=25, command=self.limpar_texto)
         self.botao_limpar.place(x=120, y=190)
         
-    
-    
-    
+    def mostrarEstadoImc(self):
+        lista_peso = ['','abaixo do peso',"peso normal","sobrepeso","obesidade grau I","obesidade grau II","obesidade grau III"] 
+        self.classificarImc()
+        try:
+            self.most_imc = CTkLabel(self.frame_baixo, text=lista_peso[self.estadoImc],width=220, height=80, font=('Arial',20))
+            self.most_imc.place(x=2,y=5)
+        except:
+            pass 
     def calcularImc(self):
         try:
             self.alturab = self.pegar_altura.get()
@@ -57,23 +62,38 @@ class Janela:
             pass
         
     def classificarImc(self):
+        self.estadoImc = None
         self.calcularImc()
-        if self.imc < 18.5:
-            print( "Abaixo do peso")
-        elif 18.5 <= self.imc < 24.9:
-            print( "Peso normal")
-        elif 25 <= self.imc < 29.9:
-            print( "Sobrepeso")
-        elif 30 <= self.imc < 34.9:
-            print( "Obesidade grau I")
-        elif 35 <= self.imc < 39.9:
-            print( "Obesidade grau II")
-        else:
-            print( "Obesidade grau III")
+        try:
+            if self.imc < 18.5:
+                self.estadoImc = 1
+                return self.estadoImc
+            elif 18.5 <= self.imc < 24.9:
+                self.estadoImc = 2
+                return self.estadoImc
+            elif 25 <= self.imc < 29.9:
+                self.estadoImc = 3
+                return self.estadoImc
+            elif 30 <= self.imc < 34.9:
+                self.estadoImc = 4
+                return self.estadoImc
+            elif 35 <= self.imc < 39.9:
+                self.estadoImc = 5
+                return self.estadoImc
+            else:
+                self.estadoImc = 6
+                return self.estadoImc
+        except:
+            self.most_imc = CTkLabel(self.frame_baixo, text='Você não digitou nada!',width=220, height=80)
+            self.most_imc.place(x=3,y=5)
+            
         
     def limpar_texto(self):
         self.pegar_peso = CTkEntry(self.frame, width=120,border_color='#d0e0eb', placeholder_text=self.texto_peso)
         self.pegar_peso.place(x=50, y=140)
+        
+        self.most_imc = CTkLabel(self.frame_baixo, text='',width=220, height=80)
+        self.most_imc.place(x=3,y=5)
         
         self.pegar_altura = CTkEntry(self.frame, width=120, placeholder_text=self.texto_altura,border_color='#d0e0eb')
         self.pegar_altura.place(x=50, y=70)
